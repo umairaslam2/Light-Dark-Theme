08d3eb699c0506c54c779bd7ac6ff4ac49245ce0
Muhammad Umair Aslam <umairaslamumer@gmail.com>
2024-11-09 04:30:40 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import LightAndDarkModeButton from './components/LightAndDarkModeButton'
+
+const App = () => {
+  return (
+    <div>
+
+    
+    <LightAndDarkModeButton/>
+      
+    </div>
+  )
+}
+
+export default App
